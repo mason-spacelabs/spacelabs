9bd9e61082b0e2a9bed2c97c5e32c32f19cc8616
Mason Halstead <halstemx@NA.OSIENT.COM>
2018-02-20 16:52:23 -0800

    Product specifications solution implemented

---
+++ spacelabs-dev/assets/specifications_data.js
@@ -0,0 +1,92 @@
+const data = {
+    "956245311531": {
+        "10153058697259": {
+            "Specifications": "NN1ST-M316-10",
+            "Country": "Africa",
+            "City": "Nairobi",
+            "Population": "3.375m",
+            "Democracy": "True",
+            "Verified": "True"
+        },
+        "10153058730027": {
+            "Specifications": "NN2ST-M316-10",
+            "Country": "Africa",
+            "City": "Nairobi",
+            "Population": "3.375m",
+            "Democracy": "True",
+            "Verified": "True"
+        },
+        "10153058828331": {
+            "Specifications": "NN3ST-M316-10",
+            "Country": "Africa",
+            "City": "Nairobi",
+            "Population": "3.375m",
+            "Democracy": "True",
+            "Verified": "True"
+        },
+        "10153058861099": {
+            "Specifications": "NN4ST-M316-10",
+            "Country": "Africa",
+            "City": "Nairobi",
+            "Population": "3.375m",
+            "Democracy": "True",
+            "Verified": "True"
+        }
+    },
+    "964084072491": {
+        "10232430755883": {
+            "Specifications": "ST0814HP-05",
+            "Size": "Infant",
+            "City": "Nairobi",
+            "Population": "3.375m",
+            "Democracy": "True",
+            "Verified": "True"
+        },
+        "10232430788651": {
+            "Specifications": "ST1320HP-05",
+            "Size": "Child",
+            "City": "Nairobi",
+            "Population": "3.375m",
+            "Democracy": "True",
+            "Verified": "True"
+        },
+        "10232430821419": {
+            "Specifications": "ST1826HP-05",
+            "Size": "Adult",
+            "City": "Nairobi",
+            "Population": "3.375m",
+            "Democracy": "True",
+            "Verified": "True"
+        },
+        "10232430854187": {
+            "Specifications": "ST2635HP-05",
+            "Size": "Adult Long",
+            "City": "Nairobi",
+            "Population": "3.375m",
+            "Democracy": "True",
+            "Verified": "True"
+        }
+    },"964139221035": {
+        "10233012781099": {
+            "Specifications": "714-0019-01",
+            "Compatibility": "Most Monitors",
+            "Reusable": "Yes",
+            "CE Certified": "Yes",
+            "Natural Rubber": "No",
+            "Sterile": "No",
+            "Patient Type": "Neonatal",
+            "Connector Type": "Safety"
+        }
+    },"964152000555": {
+        "10233038077995": {
+            "Specifications": "714-0018-00",
+            "Compatibility": "Most Monitors",
+            "Reusable": "Yes",
+            "CE Certified": "Yes",
+            "Natural Rubber": "No",
+            "Sterile": "No",
+            "Patient Type": "Adult, Pediatric",
+            "Connector Type": "Quick Release"
+        }
+    }
+};
