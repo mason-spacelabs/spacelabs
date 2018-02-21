784d0953b2eaf4ecd95386c53c522823daf9dcd4
Mason Halstead <halstemx@NA.OSIENT.COM>
2018-02-21 14:52:11 -0800

    Documentation + Specification implemented with fake data

--- spacelabs-dev/assets/specifications_data.js
+++ spacelabs-dev/assets/specifications_data.js
@@ -1,92 +1,435 @@
 const data = {
-    "956245311531": {
-        "10153058697259": {
-            "Specifications": "NN1ST-M316-10",
-            "Country": "Africa",
-            "City": "Nairobi",
-            "Population": "3.375m",
-            "Democracy": "True",
-            "Verified": "True"
+	"956245311531": {
+		"specifications": {
+			"Compatibility": "Most Monitors",
+			"Reusable": "No",
+			"CE Certified": "Yes",
+			"Natural Rubber": "No",
+			"Sterile": "No",
+			"Patient Type": "Neonatal",
+			"Hose Type": "Single Hose",
+			"Connector Type": "Male Luer 3/16"
         },
-        "10153058730027": {
-            "Specifications": "NN2ST-M316-10",
-            "Country": "Africa",
-            "City": "Nairobi",
-            "Population": "3.375m",
-            "Democracy": "True",
-            "Verified": "True"
+        "documentation": {
+			"NN1ST-M316-10 Documentation.pdf": "/path-name-here",
+            "NN2ST-M316-10 Documentation.pdf": "/path-name-here",
+            "NN3ST-M316-10 Documentation.pdf": "/path-name-here",
+            "NN4ST-M316-10 Documentation.pdf": "/path-name-here",
+            "NN5ST-M316-10 Documentation.pdf": "/path-name-here"
+		},
+		"variants": {
+			"10153058697259": {
+				"Product SKU": "NN1ST-M316-10",
+				"Size": "1",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10153058730027": {
+				"Product SKU": "NN2ST-M316-10",
+				"Size": "2",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10153058762795": {
+				"Product SKU": "NN3ST-M316-10",
+				"Size": "3",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10153058828331": {
+				"Product SKU": "NN4ST-M316-10",
+				"Size": "4",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10153058861099": {
+				"Product SKU": "NN5ST-M316-10",
+				"Size": "5",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			}
+		}
+	},
+	"964084072491": {
+		"specifications": {
+			"Compatibility": "Most Monitors",
+			"Reusable": "No",
+			"CE Certified": "Yes",
+			"Natural Rubber": "No",
+			"Sterile": "No",
+			"Patient Type": "Neonatal",
+			"Hose Type": "Single Hose"
         },
-        "10153058828331": {
-            "Specifications": "NN3ST-M316-10",
-            "Country": "Africa",
-            "City": "Nairobi",
-            "Population": "3.375m",
-            "Democracy": "True",
-            "Verified": "True"
+        "documentation": {
+			"ST0814HP-05 Documentation.pdf": "/path-name-here",
+            "ST1320HP-05 Documentation.pdf": "/path-name-here",
+            "ST1826HP-05 Documentation.pdf": "/path-name-here",
+            "ST2635HP-05 Documentation.pdf": "/path-name-here",
+            "ST2938HP-05 Documentation.pdf": "/path-name-here",
+            "ST3242HP-05 Documentation.pdf": "/path-name-here",
+            "ST3544HP-05 Documentation.pdf": "/path-name-here",
+            "ST4250HP-05 Documentation.pdf": "/path-name-here"
+		},
+		"variants": {
+			"10232430755883": {
+				"Product SKU": "ST0814HP-05",
+				"Size": "Infant",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232430788651": {
+				"Product SKU": "ST1320HP-05",
+				"Size": "Child",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232430821419": {
+				"Product SKU": "ST1826HP-05",
+				"Size": "Small Adult",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232430854187": {
+				"Product SKU": "ST2635HP-05",
+				"Size": "Adult",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232430886955": {
+				"Product SKU": "ST2938HP-05",
+				"Size": "Adult Long",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232639225899": {
+				"Product SKU": "ST3242HP-05",
+				"Size": "Large Adult",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10320193585195": {
+				"Product SKU": "ST3544HP-05",
+				"Size": "Large Adult Long",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10320260956203": {
+				"Product SKU": "ST4250HP-05",
+				"Size": "Thigh",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			}
+		}
+	},
+	"964139221035": {
+		"specifications": {
+			"Compatibility": "Most Monitors",
+			"Reusable": "Yes",
+			"CE Certified": "Yes",
+			"Natural Rubber": "No",
+			"Sterile": "No",
+			"Patient Type": "Neonatal",
+			"Connector Type": "Safety"
         },
-        "10153058861099": {
-            "Specifications": "NN4ST-M316-10",
-            "Country": "Africa",
-            "City": "Nairobi",
-            "Population": "3.375m",
-            "Democracy": "True",
-            "Verified": "True"
-        }
-    },
-    "964084072491": {
-        "10232430755883": {
-            "Specifications": "ST0814HP-05",
-            "Size": "Infant",
-            "City": "Nairobi",
-            "Population": "3.375m",
-            "Democracy": "True",
-            "Verified": "True"
+        "documentation": {
+			"714-0019-01 Documentation.pdf": "/path-name-here"
+		},
+		"variants": {
+			"10233012781099": {
+				"Product SKU": "714-0019-01",
+				"Size": "1",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			}
+		}
+	},
+	"964152000555": {
+		"specifications": {
+			"Compatibility": "Most Monitors",
+			"Reusable": "Yes",
+			"CE Certified": "Yes",
+			"Natural Rubber": "No",
+			"Sterile": "No",
+			"Patient Type": "Adult, Pediatric",
+			"Connector Type": "Quick Release"
         },
-        "10232430788651": {
-            "Specifications": "ST1320HP-05",
-            "Size": "Child",
-            "City": "Nairobi",
-            "Population": "3.375m",
-            "Democracy": "True",
-            "Verified": "True"
+        "documentation": {
+			"714-0018-00 Documentation.pdf": "/path-name-here"
+		},
+		"variants": {
+			"10233038077995": {
+				"Product SKU": "714-0018-00",
+				"Size": "1",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			}
+		}
+	},
+	"964080140331": {
+		"specifications": {
+			"Compatibility": "Most Monitors",
+			"Reusable": "No",
+			"CE Certified": "Yes",
+			"Natural Rubber": "No",
+			"Sterile": "No",
+			"Patient Type": "Neonatal",
+			"Hose Type": "Single Hose",
+			"Connector Type": "Male Luer 3/16"
         },
-        "10232430821419": {
-            "Specifications": "ST1826HP-05",
-            "Size": "Adult",
-            "City": "Nairobi",
-            "Population": "3.375m",
-            "Democracy": "True",
-            "Verified": "True"
+        "documentation": {
+			"VNN1ST-M316-10 Documentation.pdf": "/path-name-here",
+            "VNN2ST-M316-10 Documentation.pdf": "/path-name-here",
+            "VNN3ST-M316-10 Documentation.pdf": "/path-name-here",
+            "VNN4ST-M316-10 Documentation.pdf": "/path-name-here",
+            "VNN5ST-M316-10 Documentation.pdf": "/path-name-here"
+		},
+		"variants": {
+			"10232382881835": {
+				"Product SKU": "VNN1ST-M316-10",
+				"Size": "1",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232382914603": {
+				"Product SKU": "VNN2ST-M316-10",
+				"Size": "2",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232382947371": {
+				"Product SKU": "VNN3ST-M316-10",
+				"Size": "3",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232382980139": {
+				"Product SKU": "VNN4ST-M316-10",
+				"Size": "4",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232383012907": {
+				"Product SKU": "VNN5ST-M316-10",
+				"Size": "5",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			}
+		}
+	},
+	"964124639275": {
+		"specifications": {
+			"Compatibility": "Most Monitors",
+            "Reusable": "Yes",
+            "CE Certified": "Yes",
+            "Natural Rubber": "No",
+            "Sterile": "No",
+            "Patient Type": "Neonatal",
+            "Hose Type": "Single Hose",
+            "Connector Type": "N/A"
         },
-        "10232430854187": {
-            "Specifications": "ST2635HP-05",
-            "Size": "Adult Long",
-            "City": "Nairobi",
-            "Population": "3.375m",
-            "Democracy": "True",
-            "Verified": "True"
-        }
-    },"964139221035": {
-        "10233012781099": {
-            "Specifications": "714-0019-01",
+        "documentation": {
+			"US0814MHP-05 Documentation.pdf": "/path-name-here",
+            "US1320MHP-05 Documentation.pdf": "/path-name-here",
+            "US1826MHP-05 Documentation.pdf": "/path-name-here",
+            "US2635MHP-05 Documentation.pdf": "/path-name-here",
+            "US2938MHP-05 Documentation.pdf": "/path-name-here",
+            "US3242MHP-05 Documentation.pdf": "/path-name-here",
+            "US3544MHP-05 Documentation.pdf": "/path-name-here",
+            "US4250MHP-05 Documentation.pdf": "/path-name-here"
+		},
+		"variants": {
+			"10232765841451": {
+				"Product SKU": "US0814MHP-05",
+				"Size": "Infant",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232765874219": {
+				"Product SKU": "US1320MHP-05",
+				"Size": "Child",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232765906987": {
+				"Product SKU": "US1826MHP-05",
+				"Size": "Small Adult",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232765939755": {
+				"Product SKU": "US2635MHP-05",
+				"Size": "Adult",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232765972523": {
+				"Product SKU": "US2938MHP-05",
+				"Size": "Adult Long",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10325361262635": {
+				"Product SKU": "US3242MHP-05",
+				"Size": "Large Adult",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10325353300011": {
+				"Product SKU": "US3544MHP-05",
+				"Size": "Large Adult Long",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232766005291": {
+				"Product SKU": "US4250MHP-05",
+				"Size": "Thigh",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			}
+		}
+	},
+	"964116906027": {
+		"specifications": {
             "Compatibility": "Most Monitors",
             "Reusable": "Yes",
             "CE Certified": "Yes",
             "Natural Rubber": "No",
             "Sterile": "No",
             "Patient Type": "Neonatal",
-            "Connector Type": "Safety"
-        }
-    },"964152000555": {
-        "10233038077995": {
-            "Specifications": "714-0018-00",
-            "Compatibility": "Most Monitors",
-            "Reusable": "Yes",
-            "CE Certified": "Yes",
-            "Natural Rubber": "No",
-            "Sterile": "No",
-            "Patient Type": "Adult, Pediatric",
-            "Connector Type": "Quick Release"
-        }
-    }
+            "Hose Type": "Single Hose",
+            "Connector Type": "N/A"
+        },
+        "documentation": {
+			"US0814HP-05 Documentation.pdf": "/path-name-here",
+            "US1320HP-05 Documentation.pdf": "/path-name-here",
+            "US1826HP-05 Documentation.pdf": "/path-name-here",
+            "US2635HP-05 Documentation.pdf": "/path-name-here",
+            "US2938HP-05 Documentation.pdf": "/path-name-here",
+            "US3242HP-05 Documentation.pdf": "/path-name-here",
+            "US3544HP-05 Documentation.pdf": "/path-name-here",
+            "US4250HP-05 Documentation.pdf": "/path-name-here"
+		},
+		"variants": {
+			"10232695521323": {
+				"Product SKU": "US0814HP-05",
+				"Size": "Infant",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232695554091": {
+				"Product SKU": "US1320HP-05",
+				"Size": "Child",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232695619627": {
+				"Product SKU": "US1826HP-05",
+				"Size": "Small Adult",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232695652395": {
+				"Product SKU": "US2635HP-05",
+				"Size": "Adult",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232695750699": {
+				"Product SKU": "US2938HP-05",
+				"Size": "Adult Long",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10325224292395": {
+				"Product SKU": "US3242HP-05",
+				"Size": "Large Adult",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10325216100395": {
+				"Product SKU": "US3544HP-05",
+				"Size": "Large Adult Long",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232695783467": {
+				"Product SKU": "US4250HP-05",
+				"Size": "Thigh",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			}
+		}
+	},
+	"964129161259": {
+		"specifications": {
+			"Compatibility": "Most Monitors",
+			"Reusable": "No",
+			"CE Certified": "Yes",
+			"Natural Rubber": "No",
+			"Sterile": "No",
+			"Patient Type": "Neonatal",
+			"Hose Type": "Single Hose",
+			"Connector Type": "N/A"
+        },
+        "documentation": {
+			"VST0814HP-05 Documentation.pdf": "/path-name-here",
+            "VST1320HP-05 Documentation.pdf": "/path-name-here",
+            "VST1826HP-05 Documentation.pdf": "/path-name-here",
+            "VST2635HP-05 Documentation.pdf": "/path-name-here",
+            "VST2938HP-05 Documentation.pdf": "/path-name-here",
+            "VST3242HP-05 Documentation.pdf": "/path-name-here",
+            "VST3544HP-05 Documentation.pdf": "/path-name-here",
+            "VST4250HP-05 Documentation.pdf": "/path-name-here"
+		},
+		"variants": {
+			"10232817221675": {
+				"Product SKU": "VST0814HP-05",
+				"Size": "Infant",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232817254443": {
+				"Product SKU": "VST1320HP-05",
+				"Size": "Child",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232817287211": {
+				"Product SKU": "VST1826HP-05",
+				"Size": "Small Adult",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232817319979": {
+				"Product SKU": "VST2635HP-05",
+				"Size": "Adult",
+                "Limb Circumference": "N/A",
+                "Packaging": "10/EA"
+			},
+			"10232817352747": {
+				"Product SKU": "VST2938HP-05",
+				"Size": "Adult Long",
+                "Limb Circumference": "N/A",
+                "Packaging": "5/EA"
+			},
+			"10324494680107": {
+				"Product SKU": "VST3242HP-05",
+				"Size": "Large Adult",
+                "Limb Circumference": "N/A",
+                "Packaging": "5/EA"
+			},
+			"10324522008619": {
+				"Product SKU": "VST3544HP-05",
+				"Size": "Large Adult Long",
+                "Limb Circumference": "N/A",
+                "Packaging": "5/EA"
+			},
+			"10232817385515": {
+				"Product SKU": "VST4250HP-05",
+				"Size": "Thigh",
+                "Limb Circumference": "N/A",
+                "Packaging": "5/EA"
+			}
+		}
+	}
 };
